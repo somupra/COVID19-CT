78c32ccff6e80bf27e3031f240b1a41f413686aa
Somu Prajapati <somupra9@gmail.com>
2020-04-17 20:01:40 +0530

    added verbose in preprocess.sh

--- processing-and-visualisation/preprocess.sh
+++ processing-and-visualisation/preprocess.sh
@@ -4,6 +4,7 @@ ORIGINAL_IDS=$(cut -d , -f 1 $1 | awk '!a[$0]++')
 NEW_ID=0
 
 while IFS= read -r ID; do
+    echo "Replacing $ID -> $NEW_ID"
     sed -i "s/^$ID,/$NEW_ID,/g" $1
     NEW_ID=$((NEW_ID+1))
 done <<< "$ORIGINAL_IDS"
