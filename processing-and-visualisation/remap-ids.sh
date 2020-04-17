945518627e4b483df8dcc61ea64b89eeb4e43a67
Somu Prajapati <somupra9@gmail.com>
2020-04-17 17:41:44 +0530

    remap id file added

---
+++ processing-and-visualisation/remap-ids.sh
@@ -0,0 +1,10 @@
+ORIGINAL_IDS=$(cat $1 | cut -d , -f 1 | awk '!a[$0]++')
+
+NEW_ID=0
+
+while IFS= read -r ID; do
+
+    cat $1 | grep ^$ID, | sed "s/^$ID,/$NEW_ID,/g" >> "$1_remapped.csv"
+    NEW_ID=$((NEW_ID+1))
+
+done <<< "$ORIGINAL_IDS"
