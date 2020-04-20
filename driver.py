91a653ea1ddc82649c3378ba16d84faf36775d64
Somu <somupra9@gmail.com>
2020-04-20 16:06:53 +0000

    final simulation code, running for a population of 5000

--- driver.py
+++ driver.py
@@ -1,5 +1,6 @@
 from simulation import simulate
 
-simulate(path="../openpflow/sample.csv", population=100, days=100, tstamp_per_day=40)
-simulate(path="../openpflow/sample.csv", population=100, days=100, tstamp_per_day=40, algo_mode='level0')
+simulate(path="../openpflow/final.csv", population=5000, days=100, tstamp_per_day=240)
+simulate(path="../openpflow/final.csv", population=5000, days=100, tstamp_per_day=240, algo_mode='level0')
 #simulate("../openpflow/final.csv")
+simulate(path="../openpflow/final.csv", population=5000, days=100, tstamp_per_day=240, algo_mode='level1')
