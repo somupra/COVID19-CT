ceaca68dc7d88ba3460db03ffea453027f9dcfcf
Somu <somupra9@gmail.com>
2020-04-20 06:27:44 +0000

    testing on sample data, deployment fix in sampling syntax

--- driver.py
+++ driver.py
@@ -1,3 +1,4 @@
 from simulation import simulate
 
-simulate("../openpflow/final.csv")
+simulate(path="../openpflow/sample.csv", population=100, days=100, tstamp_per_day=40)
+#simulate("../openpflow/final.csv")
--- params.py
+++ params.py
@@ -1,11 +1,11 @@
 # Radius of contact: in which 2 people will be assumed to have a contact
-RADIUS = 30
+RADIUS = 20
 
 # Initially, for the 5 days, sample of population which is to be infected
-INITIAL_INF_POP = 100
+INITIAL_INF_POP = 10
 
 # Chunk size whcih needs to be set while reading the CSV file
-C_SIZE = 10 ** 8
+C_SIZE = 400000
 
 # Threshold of probabitlity of infection above which a node would be isolated
-ISOLATION_PROB_THRESHOLD = 0.75
+ISOLATION_PROB_THRESHOLD = 0.6
