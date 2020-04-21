a44bc0140153309c98449d36ddb7a204629ffda4
Somu Prajapati <somupra9@gmail.com>
2020-04-21 13:34:03 +0530

    debugging infect_func()

--- prob_attach.py
+++ prob_attach.py
@@ -1,6 +1,8 @@
 def attach_prob(src, trg, curr_day):
     """Attaches the probability to the nodes wrt the source they got infected [assumed to be]. For now, analysis done on the basis of number of contacts the source made with the target node. [NOTE]: The target node might infect the source node as well, as well as, there might be more than one source infecting the same target, we add up the probabilities, and finally trim it if it exceeds 1."""
     contact_times = len([contact for contact in trg.edge_dict[src.id] if (contact[0] // 1000) == curr_day])
+    print([contact for contact in trg.edge_dict[src.id]])
+    print([contact for contact in trg.edge_dict[src.id] if (contact[0] // 1000) == curr_day])
     print("contacts between:", src, " -- ", trg, " : ", contact_times)
 
     if contact_times < 3 and contact_times >= 1:
