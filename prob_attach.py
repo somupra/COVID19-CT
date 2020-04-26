6b81586b26ca721a0fa7059a3d003c72fa7fd495
Somu Prajapati <somupra9@gmail.com>
2020-04-26 12:46:11 +0530

    fixed fstring syntax for python 3.5 support on the server

--- prob_attach.py
+++ prob_attach.py
@@ -2,7 +2,7 @@ from params import RADIUS
 def attach_prob(src, trg, curr_day, city):
     """Attaches the probability to the nodes wrt the source they got infected [assumed to be]. For now, analysis done on the basis of number of contacts the source made with the target node. [NOTE]: The target node might infect the source node as well, as well as, there might be more than one source infecting the same target, we add up the probabilities, and finally trim it if it exceeds 1."""
 
-    print(f"Day {curr_day} :: attaching prob for source ({src}, {src.status}) and ({trg}, {trg.status})")
+    print("Day {0} :: attaching prob for source ({1}, {2}) and ({3}, {4})".format(curr_day, src, src.status, trg, trg.status))
     print("reversed edge dict is: ", reversed(trg.edge_dict[src.id]))
 
     if src.inf_start_time and not trg.is_infected():
